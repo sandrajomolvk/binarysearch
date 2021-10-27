83a8d226d8a70931bc82cea54d4946386b6bfd65
sandrajomolvk <56868446+sandrajomolvk@users.noreply.github.com>
2021-10-27 18:03:24 +0530

    Update binary.py

--- binary.py
+++ binary.py
@@ -9,9 +9,15 @@ def binarysearch(a, l, r, x):
         else:
             return binarysearch(a, mid + 1, r, x)
     else:
-        return -1
+        l=len(a)
+        if(p>a[l-1]):
+            print(p,"is not found in the array \n It can be inserted at index",p,"can be inserted at index",l)
+        else:
+            for c in range(l):
+                if(a[c]>p):
+                    print(p,"is not found in the array \n",p, "It can be inserted at index",c)
+                    break
        
- 
 a=[]
 num=int(input("Enter the number of elements in the array : "))
 print("Input the elements")
@@ -23,17 +29,6 @@ print(a)
 s=len(a)-1
 p=int(input("Enter the value of p - "))
 x=binarysearch(a,0,s,p)
-if x!=-1:
+if x:
     print(p,"is found in the array at index ",x)
-else:
-    print("  Element not found")
-   
-    z=len(a)
-    if(p>a[z-1]):
-        print(p,"can be inserted at index",z)
-    else:
-        for c in range(z):
-           if(a[c]>p):
-              print(p," can be inserted at index ",c)
-              break
     
